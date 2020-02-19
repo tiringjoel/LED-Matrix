52cfbaac380c2b679563a3aeb2a45da040e63a0e
tiringjoel <45256179+tiringjoel@users.noreply.github.com>
2020-02-19 23:18:18 +0100

    Added maxdriver for BeagleBoneBlack
    Pending: Renew Code to write Pins on BBB

---
+++ MAXDRIVER/BeagleBoneBlack/bbbmaxdriver.py
@@ -0,0 +1,48 @@
+from ctypes import *
+import numpy as np
+import numpy.ctypeslib as npct
+import os
+
+maxdriverlib = cdll.LoadLibrary(os.path.abspath("maxdriver.so"))
+array_1d_uint8 = npct.ndpointer(dtype=np.uint8, ndim=1, flags='CONTIGUOUS')
+
+def wrap_function(funcname, restype, argtypes):
+    """Simplify wrapping ctypes functions"""
+    func = maxdriverlib.__getattr__(funcname)
+    func.restype = restype
+    func.argtypes = argtypes
+    return func
+
+class MAX(Structure):
+    _fields_ = [
+        ("clkpin",c_uint8),
+        ("cspin",c_uint8),
+        ("datapin",c_uint8),
+        ("nrmax",c_uint8)
+    ]
+
+    def __init__(self, clkpin, cspin, datapin, nrmax):
+        self.clkpin = clkpin
+        self.cspin = cspin
+        self.datapin = datapin
+        self.nrmax = nrmax
+        self.initMax = wrap_function('initMax',None,[POINTER(MAX), c_uint8, c_uint8, c_uint8, c_uint8])
+        # self = initMax(self,clkpin, cspin, datapin, nrmax)
+        self.initMax(self,clkpin, cspin, datapin, nrmax)
+        self.shiftBufferOut_func = wrap_function('shiftBufferOut',None,[POINTER(MAX), array_1d_uint8])
+        self.clearDisplay_func = wrap_function('clearDisplay',None, [POINTER(MAX)])
+
+    def shiftBufferOut(self, buffer):
+        self.shiftBufferOut_func(self, buffer)
+
+    def clearDisplay(self):
+        self.clearDisplay_func(self)
+
+
+# # Example
+# print("----------------MAX1----------------")
+# mymax = MAX(10,20,35,4)
+# print("----------------MAX2----------------")
+# mymax2 = MAX(99,88,77,2)
+# x = np.arange(0,256,1,dtype = np.uint8)
+# mymax.shiftBufferOut(x)
