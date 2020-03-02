a9ca72be928154fa370cd9d671a859a4168ddd21
Unknown <joel.wirz@students.fhnw.ch>
2020-03-02 19:31:05 +0100

    style changes in bbbmaxdriver.py

--- MAXDRIVER/BeagleBoneBlack/utils/bbbmaxdriver.py
+++ MAXDRIVER/BeagleBoneBlack/utils/bbbmaxdriver.py
@@ -1,9 +1,9 @@
-from ctypes import *
+from ctypes as ct
 import numpy as np
 import numpy.ctypeslib as npct
 import os
 
-maxdriverlib = cdll.LoadLibrary(os.path.abspath("libmaxdriver.so"))
+maxdriverlib = ct.cdll.LoadLibrary(os.path.abspath("libmaxdriver.so"))
 array_1d_uint8 = npct.ndpointer(dtype=np.uint8, ndim=2, flags='CONTIGUOUS')
 
 def wrap_function(funcname, restype, argtypes):
@@ -13,12 +13,12 @@ def wrap_function(funcname, restype, argtypes):
     func.argtypes = argtypes
     return func
 
-class MAX(Structure):
+class Max(ct.Structure):
     _fields_ = [
-        ("clkpin",c_uint8),
-        ("cspin",c_uint8),
-        ("datapin",c_uint8),
-        ("nrmax",c_uint8)
+        ("clkpin",ct.c_uint8),
+        ("cspin",ct.c_uint8),
+        ("datapin",ct.c_uint8),
+        ("nrmax",ct.c_uint8)
     ]
 
     def __init__(self, clkpin, cspin, datapin, nrmax):
@@ -26,11 +26,10 @@ class MAX(Structure):
         self.cspin = cspin
         self.datapin = datapin
         self.nrmax = nrmax
-        self.initMax = wrap_function('initMax',None,[POINTER(MAX), c_uint8, c_uint8, c_uint8, c_uint8])
-        # self = initMax(self,clkpin, cspin, datapin, nrmax)
+        self.initMax = wrap_function('initMax',None,[ct.Pointer(Max), ct.c_uint8, ct.c_uint8, ct.c_uint8, ct.c_uint8])
         self.initMax(self,clkpin, cspin, datapin, nrmax)
-        self.shiftBufferOut_func = wrap_function('shiftBufferOut',None,[POINTER(MAX), array_1d_uint8])
-        self.clearDisplay_func = wrap_function('clearDisplay',None, [POINTER(MAX)])
+        self.shiftBufferOut_func = wrap_function('shiftBufferOut',None,[ct.Pointer(Max), array_1d_uint8])
+        self.clearDisplay_func = wrap_function('clearDisplay',None, [ct.Pointer(Max)])
 
     def shiftBufferOut(self, buffer):
         self.shiftBufferOut_func(self, buffer)
